285c0b6a990057729251b9696351ef47b0054735
will <williampaulgeorge@outlook.com>
2022-09-13 17:30:47 +0100

    menu changes

--- spg/setup.Designer.cs
+++ spg/setup.Designer.cs
@@ -32,20 +32,21 @@ namespace spg
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(setup));
             this.itemList = new System.Windows.Forms.ListBox();
             this.createBox = new System.Windows.Forms.GroupBox();
+            this.lengthInfoBox = new System.Windows.Forms.RichTextBox();
+            this.lengthBox = new System.Windows.Forms.TextBox();
+            this.lengthLabel = new System.Windows.Forms.Label();
+            this.industryBox = new System.Windows.Forms.TextBox();
+            this.industryLabel = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.removeBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
             this.vehicleRBtn = new System.Windows.Forms.RadioButton();
             this.sidingRBtn = new System.Windows.Forms.RadioButton();
-            this.addBtn = new System.Windows.Forms.Button();
-            this.removeBtn = new System.Windows.Forms.Button();
             this.editLabel = new System.Windows.Forms.Label();
-            this.nameLabel = new System.Windows.Forms.Label();
-            this.nameBox = new System.Windows.Forms.TextBox();
-            this.industryLabel = new System.Windows.Forms.Label();
-            this.industryBox = new System.Windows.Forms.TextBox();
-            this.lengthLabel = new System.Windows.Forms.Label();
-            this.lengthBox = new System.Windows.Forms.TextBox();
-            this.lengthInfoBox = new System.Windows.Forms.RichTextBox();
             this.saveBtn = new System.Windows.Forms.Button();
             this.infoBox = new System.Windows.Forms.RichTextBox();
+            this.errorLabel = new System.Windows.Forms.Label();
             this.createBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,6 +62,7 @@ namespace spg
             // 
             // createBox
             // 
+            this.createBox.Controls.Add(this.errorLabel);
             this.createBox.Controls.Add(this.lengthInfoBox);
             this.createBox.Controls.Add(this.lengthBox);
             this.createBox.Controls.Add(this.lengthLabel);
@@ -79,6 +81,86 @@ namespace spg
             this.createBox.TabStop = false;
             this.createBox.Text = "create new";
             // 
+            // lengthInfoBox
+            // 
+            this.lengthInfoBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.lengthInfoBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lengthInfoBox.Location = new System.Drawing.Point(9, 171);
+            this.lengthInfoBox.Name = "lengthInfoBox";
+            this.lengthInfoBox.Size = new System.Drawing.Size(361, 95);
+            this.lengthInfoBox.TabIndex = 8;
+            this.lengthInfoBox.Text = resources.GetString("lengthInfoBox.Text");
+            // 
+            // lengthBox
+            // 
+            this.lengthBox.Location = new System.Drawing.Point(9, 142);
+            this.lengthBox.Name = "lengthBox";
+            this.lengthBox.Size = new System.Drawing.Size(100, 22);
+            this.lengthBox.TabIndex = 7;
+            // 
+            // lengthLabel
+            // 
+            this.lengthLabel.AutoSize = true;
+            this.lengthLabel.Location = new System.Drawing.Point(10, 123);
+            this.lengthLabel.Name = "lengthLabel";
+            this.lengthLabel.Size = new System.Drawing.Size(51, 16);
+            this.lengthLabel.TabIndex = 6;
+            this.lengthLabel.Text = "length*:";
+            // 
+            // industryBox
+            // 
+            this.industryBox.Location = new System.Drawing.Point(9, 94);
+            this.industryBox.Name = "industryBox";
+            this.industryBox.Size = new System.Drawing.Size(361, 22);
+            this.industryBox.TabIndex = 5;
+            // 
+            // industryLabel
+            // 
+            this.industryLabel.AutoSize = true;
+            this.industryLabel.Location = new System.Drawing.Point(10, 74);
+            this.industryLabel.Name = "industryLabel";
+            this.industryLabel.Size = new System.Drawing.Size(85, 16);
+            this.industryLabel.TabIndex = 4;
+            this.industryLabel.Text = "industry type:";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(9, 45);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(361, 22);
+            this.nameBox.TabIndex = 3;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(10, 25);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(98, 16);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "name/identifier:";
+            // 
+            // removeBtn
+            // 
+            this.removeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.removeBtn.Location = new System.Drawing.Point(193, 272);
+            this.removeBtn.Name = "removeBtn";
+            this.removeBtn.Size = new System.Drawing.Size(177, 28);
+            this.removeBtn.TabIndex = 1;
+            this.removeBtn.Text = "remove selected";
+            this.removeBtn.UseVisualStyleBackColor = true;
+            this.removeBtn.Click += new System.EventHandler(this.removeBtn_Click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.addBtn.Location = new System.Drawing.Point(9, 272);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(177, 28);
+            this.addBtn.TabIndex = 0;
+            this.addBtn.Text = "add new";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
             // vehicleRBtn
             // 
             this.vehicleRBtn.AutoSize = true;
@@ -91,6 +173,7 @@ namespace spg
             this.vehicleRBtn.TabStop = true;
             this.vehicleRBtn.Text = "vehicles";
             this.vehicleRBtn.UseVisualStyleBackColor = true;
+            this.vehicleRBtn.CheckedChanged += new System.EventHandler(this.vehicleRBtn_CheckedChanged);
             // 
             // sidingRBtn
             // 
@@ -102,26 +185,7 @@ namespace spg
             this.sidingRBtn.TabIndex = 3;
             this.sidingRBtn.Text = "sidings";
             this.sidingRBtn.UseVisualStyleBackColor = true;
-            // 
-            // addBtn
-            // 
-            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.addBtn.Location = new System.Drawing.Point(9, 272);
-            this.addBtn.Name = "addBtn";
-            this.addBtn.Size = new System.Drawing.Size(177, 28);
-            this.addBtn.TabIndex = 0;
-            this.addBtn.Text = "add new";
-            this.addBtn.UseVisualStyleBackColor = true;
-            // 
-            // removeBtn
-            // 
-            this.removeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.removeBtn.Location = new System.Drawing.Point(193, 272);
-            this.removeBtn.Name = "removeBtn";
-            this.removeBtn.Size = new System.Drawing.Size(177, 28);
-            this.removeBtn.TabIndex = 1;
-            this.removeBtn.Text = "remove selected";
-            this.removeBtn.UseVisualStyleBackColor = true;
+            this.sidingRBtn.CheckedChanged += new System.EventHandler(this.sidingRBtn_CheckedChanged);
             // 
             // editLabel
             // 
@@ -133,64 +197,6 @@ namespace spg
             this.editLabel.TabIndex = 4;
             this.editLabel.Text = "editing:";
             // 
-            // nameLabel
-            // 
-            this.nameLabel.AutoSize = true;
-            this.nameLabel.Location = new System.Drawing.Point(10, 25);
-            this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(98, 16);
-            this.nameLabel.TabIndex = 2;
-            this.nameLabel.Text = "name/identifier:";
-            // 
-            // nameBox
-            // 
-            this.nameBox.Location = new System.Drawing.Point(9, 45);
-            this.nameBox.Name = "nameBox";
-            this.nameBox.Size = new System.Drawing.Size(361, 22);
-            this.nameBox.TabIndex = 3;
-            // 
-            // industryLabel
-            // 
-            this.industryLabel.AutoSize = true;
-            this.industryLabel.Location = new System.Drawing.Point(10, 74);
-            this.industryLabel.Name = "industryLabel";
-            this.industryLabel.Size = new System.Drawing.Size(85, 16);
-            this.industryLabel.TabIndex = 4;
-            this.industryLabel.Text = "industry type:";
-            // 
-            // industryBox
-            // 
-            this.industryBox.Location = new System.Drawing.Point(9, 94);
-            this.industryBox.Name = "industryBox";
-            this.industryBox.Size = new System.Drawing.Size(361, 22);
-            this.industryBox.TabIndex = 5;
-            // 
-            // lengthLabel
-            // 
-            this.lengthLabel.AutoSize = true;
-            this.lengthLabel.Location = new System.Drawing.Point(10, 123);
-            this.lengthLabel.Name = "lengthLabel";
-            this.lengthLabel.Size = new System.Drawing.Size(51, 16);
-            this.lengthLabel.TabIndex = 6;
-            this.lengthLabel.Text = "length*:";
-            // 
-            // lengthBox
-            // 
-            this.lengthBox.Location = new System.Drawing.Point(9, 142);
-            this.lengthBox.Name = "lengthBox";
-            this.lengthBox.Size = new System.Drawing.Size(100, 22);
-            this.lengthBox.TabIndex = 7;
-            // 
-            // lengthInfoBox
-            // 
-            this.lengthInfoBox.BackColor = System.Drawing.SystemColors.Menu;
-            this.lengthInfoBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.lengthInfoBox.Location = new System.Drawing.Point(9, 171);
-            this.lengthInfoBox.Name = "lengthInfoBox";
-            this.lengthInfoBox.Size = new System.Drawing.Size(361, 95);
-            this.lengthInfoBox.TabIndex = 8;
-            this.lengthInfoBox.Text = resources.GetString("lengthInfoBox.Text");
-            // 
             // saveBtn
             // 
             this.saveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
@@ -200,6 +206,7 @@ namespace spg
             this.saveBtn.TabIndex = 5;
             this.saveBtn.Text = "save";
             this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
             // 
             // infoBox
             // 
@@ -213,6 +220,15 @@ namespace spg
             this.infoBox.Text = "use this menu to create and remove vehicles and sidings for use in puzzle generat" +
     "ion.\nyou can switch between editing vehicles and editing sidings using the butto" +
     "ns to the right.";
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(9, 250);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(10, 16);
+            this.errorLabel.TabIndex = 9;
+            this.errorLabel.Text = " ";
             // 
             // setup
             // 
@@ -253,5 +269,6 @@ namespace spg
         private System.Windows.Forms.Label nameLabel;
         private System.Windows.Forms.Button saveBtn;
         private System.Windows.Forms.RichTextBox infoBox;
+        private System.Windows.Forms.Label errorLabel;
     }
 }
--- spg/setup.cs
+++ spg/setup.cs
@@ -13,6 +13,117 @@ namespace spg
         public setup()
         {
             InitializeComponent();
+            updateList();
+        }
+
+        private void updateList() // updated when menu first opened and as different categories are selected
+        {
+            errorLabel.Text = "";
+            itemList.BeginUpdate();
+            itemList.Items.Clear();
+            if (vehicleRBtn.Checked)
+            {
+                foreach (railVehicle vehicle in Program.vehicles)
+                {
+                    itemList.Items.Add(vehicle.ToString());
+                }
+            }
+            else if (sidingRBtn.Checked)
+            {
+                foreach (siding siding in Program.sidings)
+                {
+                    itemList.Items.Add(siding.ToString());
+                }
+            }
+            itemList.EndUpdate();
+        }
+
+        private bool validateName(string name)
+        {
+            if (name != "")
+            {
+                return true;
+            }
+            errorLabel.Text = "please enter a name";
+            return false;
+        }
+
+        private bool validateIndType(string ind)
+        {
+            if (ind != "")
+            {
+                return true;
+            }
+            errorLabel.Text = "please enter an industry type";
+            return false;
+        }
+
+        private bool validateLength(string length)
+        {
+            double intLen;
+            bool valid = double.TryParse(length, out intLen);
+            if (!valid)
+            {
+                errorLabel.Text = "length must be a number";
+            }
+            return valid;
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            if (vehicleRBtn.Checked)
+            {
+                railVehicle vehicle = new railVehicle();
+                if (validateName(nameBox.Text) && validateIndType(industryBox.Text) && validateLength(lengthBox.Text))
+                {
+                    vehicle.vID = nameBox.Text;
+                    vehicle.vIndustryType = industryBox.Text;
+                    vehicle.vLength = double.Parse(lengthBox.Text);
+                    Program.vehicles.Add(vehicle);
+                    updateList();
+                }
+            }
+            else if (sidingRBtn.Checked)
+            {
+                if (validateName(nameBox.Text) && validateIndType(industryBox.Text) && validateLength(lengthBox.Text))
+                {
+                    siding siding = new siding();
+                    siding.sID = nameBox.Text;
+                    siding.sIndustryType = industryBox.Text;
+                    siding.sLength = double.Parse(lengthBox.Text);
+                    Program.sidings.Add(siding);
+                    updateList();
+                }
+            }
+        }
+
+        private void removeBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                itemList.BeginUpdate();
+                itemList.Items.Remove(itemList.SelectedItem);
+                itemList.EndUpdate();
+            }
+            catch
+            {
+                errorLabel.Text = "error: no item selected";
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vehicleRBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            updateList();
+        }
+
+        private void sidingRBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            updateList();
         }
     }
 }
