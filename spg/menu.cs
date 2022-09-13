5d0cc9df31bf465026b01779b8a4f03cdd6b32d6
will <williampaulgeorge@outlook.com>
2022-09-13 17:30:02 +0100

    global options

--- spg/menu.cs
+++ spg/menu.cs
@@ -15,26 +15,32 @@ namespace spg
         public menu()
         {
             InitializeComponent();
+
+            // default values
+            Program.sortByIndustry = true;
+            Program.sortTrain = false;
+            trainLengthControl.Value = Program.totalVehicles;
         }
 
         private void checkSortByIndustry_CheckedChanged(object sender, EventArgs e)
         {
-            
+            Program.sortByIndustry = checkSortByIndustry.Checked;
         }
 
         private void checkSortTrain_CheckedChanged(object sender, EventArgs e)
         {
-
+            Program.sortTrain = checkSortTrain.Checked;
         }
 
         private void trainLengthControl_ValueChanged(object sender, EventArgs e)
         {
-
+            Program.totalVehicles = (int)trainLengthControl.Value;
         }
 
         private void editBtn_Click(object sender, EventArgs e)
         {
-
+            setup setupMenu = new setup();
+            setupMenu.Show();
         }
 
         private void newPuzzleBtn_Click(object sender, EventArgs e)
